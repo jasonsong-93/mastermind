1c977d2a266939738459df12b1abf8595fa65a90
Jason Song <jasonsong.93@gmail.com>
2022-02-20 18:41:39 +1100

    Updated codebreaker with tests

--- Mastermind.Tests/CodeBreakerTests.cs
+++ Mastermind.Tests/CodeBreakerTests.cs
@@ -1,7 +1,20 @@
+using Moq;
+using Xunit;
+
 namespace Mastermind.Tests
 {
     public class CodeBreakerTests
     {
-        
+        [Fact]
+        public void CodeBroken_ShouldReturnTrueIfTheCodeFromUserAndSolutionAreSame()
+        {
+            // Arrange
+            var mockSolution = new[] {Color.Black, Color.Blue, Color.Green, Color.Purple};
+            var userInputMock = new Mock<IUserInput>();
+            // Act
+            var codeBreaker = new CodeBreaker(userInputMock.Object);
+            codeBreaker.CodeBroken();
+            // Assert
+        }
     }
 }
---
+++ Mastermind/CodeBreaker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Mastermind
+{
+    public class CodeBreaker : ICodeBreaker
+
+    {
+        public CodeBreaker(IUserInput userInput)
+        {
+        }
+
+        public List<Attempt> GetGuessHistory()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CheckGuess(Color[] solution)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool CodeBroken()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
