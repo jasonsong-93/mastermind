691de446d6bab3a0309f416e20bbc2d0cf7a4e50
Jason Song <jasonsong.93@gmail.com>
2022-02-25 17:39:29 +1100

    ready for merge

--- Mastermind.Tests/CodeMakerTests.cs
+++ Mastermind.Tests/CodeMakerTests.cs
@@ -9,14 +9,11 @@ namespace Mastermind.Tests
         [Fact]
         public void GetSolutionCode_ShouldReturnACode()
         {
-            // Arrange
             var randomizerMock = new Mock<IRandomizer>(); // setup to return specific colours
             var codeMaker = new CodeMaker(randomizerMock.Object);
             randomizerMock.SetupSequence(r => r.GenerateRandomInt(Enum.GetNames(typeof(Color)).Length)).Returns(0)
                 .Returns(1).Returns(2).Returns(3);
-            // Act
             var result = codeMaker.GetSolutionCode();
-            // Assert
             Assert.Equal(new[] {Color.Red, Color.Blue, Color.Green, Color.Orange}, result);
         }
     }
