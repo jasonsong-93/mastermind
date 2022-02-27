b336e294fabc727c1fbf441563406ce07580093b
Jason Song <jasonsong.93@gmail.com>
2022-02-28 09:54:36 +1100

    Updated codemaker

---
+++ Mastermind/CodeMaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Mastermind;
+
+public class CodeMaker : ICodeMaker
+{
+    private readonly IRandomizer _randomizer;
+
+    public CodeMaker(IRandomizer randomizer)
+    {
+        _randomizer = randomizer;
+    }
+
+    public Color[] GetSolutionCode()
+    {
+    }
+}
