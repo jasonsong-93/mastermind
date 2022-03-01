fe14a50cb65d83e08c26b3630760144997c761a2
Jason Song <jason.song@myob.com>
2022-03-02 10:10:44 +1100

    Created randomizer

---
+++ Mastermind/Randomizer.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Mastermind
+{
+    class Randomizer : IRandomizer
+    {
+        private readonly Random _rand = new();
+        public int GenerateRandomInt(int maxRange)
+        {
+            
+            return _rand.Next(maxRange);
+        }
+    }
+}
